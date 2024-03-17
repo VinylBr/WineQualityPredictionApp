2fa968d3dc9b24f1b132b645a4942b05ed74485f
VinayLaptop <vinaybarnabas@gmail.com>
2024-03-17 18:23:55 +0530

    finalchange from VScode

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from joblib import dump, load
-from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler 
@@ -13,29 +13,32 @@ st.write("Hi! What factors control wine quality? Let's find out")
 
 #model = load(model_jl_file)
 
-X = pd.read_csv("data/df_X.csv")
-y = pd.read_csv("data/df_y.csv")
+X = pd.read_csv("data/X_df.csv")
+y = pd.read_csv("data/y_df.csv")
+
 max_depth = 21
-n_estimators = 61
+n_estimators = 41
 
 
-tab1, tab2 = st.tabs #tab1 for prediction and tab2 for feature importance
+tab1, tab2 = st.tabs(["Prediction", "Feature Importance"]) #tab1 for prediction and tab2 for feature importance
 
 cols_to_transform = ["residual_sugar", "free_sulfur_dioxide", "Bound_sulfur_dioxide"]
 
-st_scale  = StandardScaler()
-X = st_scale.fit_transform(features_df)
+features = X.columns.values
 
-best_forest_class = RandomForestClassifier(max_depth = max_depth, n_estimators = n_estimators)
-best_forest_class.fit(X_process_train_mouse, y_process_train_mouse)
 
 sliders = []
-for col in features_df.columns:
-    col_slider = st.slider(label = col, min_value = float(features_df[col].min()), max_value = float(features_df[col].max()), value = float(features_df[col].mean()))
+for col in features:
+    col_slider = st.slider(label = col, min_value = float(X[col].min()), max_value = float(X[col].max()), value = float(X[col].mean()))
     if col in cols_to_transform:
         col_slider = np.log(col_slider)
     sliders.append(col_slider)
 
-scaled_test_features = st_scale.transform(sliders)
+st_scale  = StandardScaler()
+X = st_scale.fit_transform(X)
+X_test = np.array(sliders).reshape(1,-1)
+best_forest_class = RandomForestClassifier(max_depth = max_depth, n_estimators = n_estimators)
+best_forest_class.fit(X, y['quality'])
+scaled_test_features = st_scale.transform(X_test)
 
-y_pred_svr = model.predict(sliders)
+y_pred_svr = best_forest_class.predict(X_test)
