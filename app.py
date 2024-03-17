3691050e50c0547d5850ad69c93623e8a9a2f7b0
VinayLaptop <vinaybarnabas@gmail.com>
2024-03-17 16:07:24 +0530

    Added pickle model

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ import pickle
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
- 
+from sklearn.preprocessing import StandardScaler 
 
 st.title("Wine Quality")
 st.write("Hi! What factors control wine quality? Let's find out")
-model_pkl_file = "wineprediction_model.pkl"
+model_pkl_file = "model/wineprediction_model.pkl"
+
 with open(model_pkl_file, 'rb') as file:
-    pickle.open(best2deploy,file)
+    model = pickle.load(file)
+
 
 features_df = pd.read_csv("data/getfeatures.csv")
 
@@ -19,14 +21,17 @@ tab1, tab2 = st.tabs #tab1 for prediction and tab2 for feature importance
 
 cols_to_transform = ["residual_sugar", "free_sulfur_dioxide", "Bound_sulfur_dioxide"]
 
-
-with tab1:
-    sliders = []
-    for col in features_df.columns:
-        col_slider = st.slider(label = col, min_value = float(features_df[col].min()), max_value = float(features_df[col].max()))
-        if col in cols_to_transform:
-           col_slider = np.log(col_slider)
-        sliders.append(col_slider)
+st_scale  = StandardScaler()
+scaled_features = st_scale.fit_transform(features_df)
 
 
-y_pred_svr = best2deploy.predict(sliders)
+sliders = []
+for col in features_df.columns:
+    col_slider = st.slider(label = col, min_value = float(features_df[col].min()), max_value = float(features_df[col].max()))
+    if col in cols_to_transform:
+        col_slider = np.log(col_slider)
+    sliders.append(col_slider)
+
+scaled_test_features = st_scale.transform(sliders)
+
+y_pred_svr = model.predict(sliders)
