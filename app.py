b0d846e778cddf12d0b1315b422e0aefe7ff0e0e
VinayLaptop <vinaybarnabas@gmail.com>
2024-03-20 23:30:06 +0530

    Added Elements to Red Wine

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from joblib import dump, load
+from sklearn.metrics.plot_confusion_matrix
 #from sklearn.ensemble import RandomForestClassifier
 #from sklearn.metrics import mean_squared_error
 #from sklearn.model_selection import GridSearchCV
@@ -54,16 +55,26 @@ prediction_prob = model.predict_proba(X_test)
 bestlabelprobability = prediction_prob[(model.classes_ == y_pred_svr).reshape(1,-1)]
 st.markdown(f"## Predicted Quality: :red[{y_pred_svr[0]}] (_>5 is Good Wine_)")
 st.markdown(f"### :blue[Confidence: {bestlabelprobability[0] :.2f}]")
-FI_jl_file = "model/feature_importance.joblib"
-RF_importance = load(FI_jl_file)
-feature_fig, ax = plt.subplots(figsize = (5,4))
-sorted_idx = RF_importance.importances_mean.argsort()
-ax.barh(pd.Series(features)[sorted_idx],
-             RF_importance.importances_mean[sorted_idx],
-             xerr = RF_importance.importances_std[sorted_idx],
-             ecolor = "yellow")
-ax.set_xlabel("Perm_importance")
-ax.set_xlim(0, 0.25)
-ax.set_title("Feature Importance")
+st.divider()
 
-st.pyplot(feature_fig)
+col1, col2 = st.columns(2)
+with col1:
+    with st.expander("What is the Feature Importance?"):
+        feature_fig, ax = plt.subplots(figsize = (5,4))
+        mean_importance = model.feature_importances_ #get feature importance based on minimum decrease in impurity
+        sorted_idx = mean_importance.argsort() #sort based on feature importance
+        std_importance = np.std([tree.feature_importances_ for tree in best_forest_reg.estimators_], axis = 0) #get standard deviation across trees
+        ax.barh(pd.Series(features)[sorted_idx],# create horizontal bar plot of sorted feature importance
+        mean_importance[sorted_idx], # mean values
+        xerr = std_importance, #std
+        ecolor = "yellow" #color ofr std wick
+        )
+        ax.set_xlabel("Perm_importance")
+        ax.set_xlim(0, 0.25)
+        ax.set_title("Feature Importance") #show the plot
+        st.pyplot(feature_fig)
+
+with col2: 
+    with st.expander("Model Performance in general"):
+        confusion_fig, ax2 = plt.subplots(figsize = (5,4))
+        plot_confusion_matrix()
